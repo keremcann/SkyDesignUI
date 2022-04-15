6904d77f52234a4631ea8a946021fb24fe4839f1
tp.mankara <tp.muharrem.ankarali@vakifkatilim.com.tr>
2022-04-15 14:30:17 +0300

    add utils

--- src/app/utils/ColumnBuilder.js
+++ src/app/utils/ColumnBuilder.js
@@ -1,11 +1,53 @@
 import React, { PureComponent } from 'react'
-import { Comparator, selectFilter, textFilter } from "react-bootstrap-table2-filter"
+import filterFatory, { Comparator, customFilter, selectFilter, textFilter } from "react-bootstrap-table2-filter"
 import { ReducerHelper } from "./ReducerHelper"
 
-export class ColumnBuilder {
+class SelectFilter extends React.Component {
+    selecty;
+    constructor(props) {
+        super(props);
 
+        this.selecty = <select
+            key="select"
+            ref={node => this.select = node}
+            className="form-control"
+            onChange={(e) => {
+                this.props.onFilter(this.select.value);
+            }}
+            onClick={e => e.stopPropagation()}
+        >
+            <option value={''}>Please select one</option>
+            {Object.entries(this.props.filterData).map(([key, value]) => {
+                return <option value={key}>{value}</option>
+            })}
+        </select>;
+    }
+
+    // getSelectQueries() {
+    //     return <select
+    //         key="select"
+    //         ref={node => this.select = node}
+    //         className="form-control"
+    //         onChange={this.onChange}
+    //         onClick={e => e.stopPropagation()}
+    //     >
+    //         <option value={''}>Please select one</option>
+    //         {Object.entries(this.props.filterData).map(([key, value]) => {
+    //             return <option value={key}>{value}</option>
+    //         })}
+    //     </select>
+    // }
+
+    render() {
+        return [
+            this.selecty
+        ];
+    }
+}
+
+export class ColumnBuilder {
     result = {
-        headerFormatter: function priceFormatter(column, colIndex, { sortElement, filterElement }) {
+        headerFormatter: function headerFormatter(column, colIndex, { sortElement, filterElement }) {
             return (
                 <div className='hovered-title'>
                     <div>{column.text} {sortElement}</div>
@@ -36,9 +78,20 @@ export class ColumnBuilder {
         this.result.idField = true;
         return this;
     }
+    isHidden() {
+        this.result.hidden = true;
+        return this;
+    }
     withSelectFilter(data) {
         this.result = {
             ...this.result,
+            // filter: customFilter(),
+            // filterRenderer: (onFilter, column) =>
+            //     <SelectFilter
+            //         onFilter={onFilter}
+            //         column={column}
+            //         filterData={ReducerHelper.mapSelectQuery(data, this.result.dataField)} />
+
             filter: selectFilter({
                 options: ReducerHelper.mapSelectQuery(data, this.result.dataField),
             })
