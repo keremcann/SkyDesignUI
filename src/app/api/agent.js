c2d2f709d26c98eb7748ceb8e18906e6e612d40a
Kerem Can <ce.cankerem@outlook.com>
2022-04-20 13:38:52 +0300

    edit service name

--- src/app/api/agent.js
+++ src/app/api/agent.js
@@ -87,7 +87,7 @@ const Page = {
     createPage: (data) => requests.post('page/createPage', data),
     getPageDetail: (level1Menu, level2Menu, level3Menu) => requests.get('page/getPageDetail?Level1Menu=' + level1Menu + '&Level2Menu=' + level2Menu + '&Level3Menu=' + level3Menu),
 
-    createContent: (data) => requests.post('page/addPageDetail', data),
+    createContent: (data) => requests.post('page/createPageDetail', data),
     updateContent: (data) => requests.post('page/updatePageDetail', data),
     deleteContent: (id) => requests.del('page/deletePageDetail?id=' + id),
 }
